542787fd7ee379e43f996cb93de30eb79489a47c
kalkaneserdar <kalkaneserdar@gmail.com>
2023-10-03 17:37:02 +0200

    change: clean up

--- js/kitty-caller-js.js
+++ js/kitty-caller-js.js
@@ -1,12 +1,11 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const url = 'https://www.marktplaats.nl/v/dieren-en-toebehoren/katten-en-kittens-raskatten-korthaar/m2027638977-britse-korthaar-x-ragdoll-kittens-all-male?utm_medium=social&utm_campaign=socialbuttons&utm_source=whatsapp-desktop'
+const url = 'https://www.example.com'
 const logFilePath = '../kitty-logger.txt'
 const maxCalls = 100
 let callsMade = 1
 
-// Make the call. Log the response code in the header and the call number in a log file with a timestamep e.g. 2019-01-01 12:00:00 - Call number 1 http_response_code: 200
 async function makeCall() {
     const browser = await puppeteer.launch({ headless: "new"});
     const page = await browser.newPage();
@@ -18,15 +17,16 @@ async function makeCall() {
     // get the http response code from the header and assign to a variable called response
     while (callsMade <= maxCalls) {
         const responseCode = (await page.goto(url)).status()
+        // WATCHPOINT: waitForTimeout() function is deprecated. An alternative has to be found.
         await page.waitForTimeout(1000)
-        // console.log(` Call number ${callsMade} - http_response_code: ${responseCode}`)
         await logMessage(callsMade, url, responseCode)
         callsMade++
     }
-    // close the browser
+    
     await browser.close();
 }
 
+// Helper function to log the message to the console and to a file
 async function logMessage(callCount, targetUrl, responseCode) {
     const message = `jsWpuppet - Call number ${callCount} ${targetUrl} - http_response_code: ${responseCode}`
     const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
